8da96c69b8f22bc751c5176998db6c277f17fac1
ysfang <ysfang82@gmail.com>
2020-03-01 15:56:42 +0800

    implement recursive

---
+++ src/main/java/dsa/playground/recursive/FindFactorial.java
@@ -0,0 +1,18 @@
+package dsa.playground.recursive;
+
+public class FindFactorial {
+    public static int findRecursively(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * findRecursively(--n);
+        }
+    }
+
+    public static int findWithLoop(int n) {
+        for (int i = n - 1; i > 0; i--) {
+            n *= i;
+        }
+        return n;
+    }
+}
---
+++ src/test/java/dsa/playground/recursive/FindFactorialTest.java
@@ -0,0 +1,25 @@
+package dsa.playground.recursive;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindFactorialTest {
+    @Test
+    public void findOneWithRecursive() {
+        assertEquals(1, FindFactorial.findRecursively(1));
+        assertEquals(2, FindFactorial.findRecursively(2));
+        assertEquals(6, FindFactorial.findRecursively(3));
+        assertEquals(24, FindFactorial.findRecursively(4));
+        assertEquals(120, FindFactorial.findRecursively(5));
+    }
+
+    @Test
+    public void findWithLoop() {
+        assertEquals(1, FindFactorial.findWithLoop(1));
+        assertEquals(2, FindFactorial.findWithLoop(2));
+        assertEquals(6, FindFactorial.findWithLoop(3));
+        assertEquals(24, FindFactorial.findWithLoop(4));
+        assertEquals(120, FindFactorial.findWithLoop(5));
+    }
+}
